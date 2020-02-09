8152fc2170804d00c2f0c257f8cc1c464822f029
nomizo-iox <nomizo.iox@gmail.com>
2020-02-09 08:51:31 -0600

    first commit

---
+++ SimpleTable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleTable
+//
+//  Created by Samuel F. Ademola on 2/9/20.
+//  Copyright © 2020 Nomizo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
