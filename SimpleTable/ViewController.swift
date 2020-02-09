9cebf81909ea3472e9588c97366337a8ed167277
nomizo-iox <nomizo.iox@gmail.com>
2020-02-09 10:09:27 -0600

    added the immages corresponding to the restaruant names.

--- SimpleTable/ViewController.swift
+++ SimpleTable/ViewController.swift
@@ -13,12 +13,19 @@ import UIKit
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     // List of Restaurants Display in a Table
-    var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "P etite Oyster",
-                          "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery" ,
-                          "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif",
-                          "Graham Av enue Meats And Deli", "Waffle & Wolf", "Five Leaves",
-                          "Cafe Lore", "Confes sional", "Barrafina", "Donostia",
-                          "Royal Oak", "CASK Pub and Kitchen"]
+    var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster",
+    "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery" ,
+    "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif",
+    "Graham Avenue Meats And Deli", "Waffle & Wolf", "Five Leaves",
+    "Cafe Lore", "Confessional", "Barrafina", "Donostia",
+    "Royal Oak", "CASK Pub and Kitchen"]
+    
+    var restaurantPics = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster",
+    "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery" ,
+    "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif",
+    "Graham Avenue Meats And Deli", "Waffle & Wolf", "Five Leaves",
+    "Cafe Lore", "Confessional", "Barrafina", "Donostia",
+    "Royal Oak", "CASK Pub and Kitchen"]
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -31,7 +38,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
         
         cell.textLabel?.text = restaurantNames[indexPath.row]
-        cell.imageView?.image = UIImage(named: "restaurant")
+        cell.imageView?.image = UIImage(named: restaurantPics[indexPath.row])
         
         return cell
     }
@@ -42,6 +49,8 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         // Do any additional setup after loading the view.
     }
 
-
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
 }
 
